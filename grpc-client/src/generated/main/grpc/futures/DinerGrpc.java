ccfd0a024c33abce2b8216afa74fa335e3e668ba
Maksym Ostroverkhov <m.ostroverkhov@gmail.com>
2025-03-28 21:11:50 +0200

    update dependencies

--- grpc-client/src/generated/main/grpc/futures/DinerGrpc.java
+++ grpc-client/src/generated/main/grpc/futures/DinerGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.66.0)",
+    value = "by gRPC proto compiler (version 1.71.0)",
     comments = "Source: service.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class DinerGrpc {
@@ -89,6 +89,21 @@ public final class DinerGrpc {
         }
       };
     return DinerStub.newStub(factory, channel);
+  }
+
+  /**
+   * Creates a new blocking-style stub that supports all types of calls on the service
+   */
+  public static DinerBlockingV2Stub newBlockingV2Stub(
+      io.grpc.Channel channel) {
+    io.grpc.stub.AbstractStub.StubFactory<DinerBlockingV2Stub> factory =
+      new io.grpc.stub.AbstractStub.StubFactory<DinerBlockingV2Stub>() {
+        @java.lang.Override
+        public DinerBlockingV2Stub newStub(io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+          return new DinerBlockingV2Stub(channel, callOptions);
+        }
+      };
+    return DinerBlockingV2Stub.newStub(factory, channel);
   }
 
   /**
@@ -186,6 +201,37 @@ public final class DinerGrpc {
 
   /**
    * A stub to allow clients to do synchronous rpc calls to service Diner.
+   */
+  public static final class DinerBlockingV2Stub
+      extends io.grpc.stub.AbstractBlockingStub<DinerBlockingV2Stub> {
+    private DinerBlockingV2Stub(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      super(channel, callOptions);
+    }
+
+    @java.lang.Override
+    protected DinerBlockingV2Stub build(
+        io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
+      return new DinerBlockingV2Stub(channel, callOptions);
+    }
+
+    /**
+     */
+    public futures.Meal order(futures.Order request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getOrderMethod(), getCallOptions(), request);
+    }
+
+    /**
+     */
+    public com.google.protobuf.Empty pay(futures.Payment request) {
+      return io.grpc.stub.ClientCalls.blockingUnaryCall(
+          getChannel(), getPayMethod(), getCallOptions(), request);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do limited synchronous rpc calls to service Diner.
    */
   public static final class DinerBlockingStub
       extends io.grpc.stub.AbstractBlockingStub<DinerBlockingStub> {
